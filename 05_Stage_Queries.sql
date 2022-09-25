344cfbc402e69ba47f5795f2bd1260606f297866
Solmak <solvy@internet.ru>
2022-09-25 14:55:02 +0300

    - Stage 05

--- 05_Stage_Queries.sql
+++ 05_Stage_Queries.sql
@@ -18,7 +18,7 @@ SELECT
 	 	FROM stars 
 	 WHERE movie_id = movies.id
 	 GROUP BY movie_id
-	) AS number_of_stars,
+	) AS number_of_ratings,
 	(SELECT (name || ' ' || surname ) 
 	 	FROM user_profiles
 	 	WHERE user_id = (SELECT user_id 
@@ -36,15 +36,94 @@ SELECT
 
 
 /*
-Найти актеров фильма, который пользователь с определенным id отметил последним.
+Найти актеров фильма, который пользователь с именем Rooney Cooley отметил последним.
 Вывести, имя и фамилию актера, фильм актера с наивысшим рейтингом, рейтинг фильма
 */
-SELECT 
+SELECT
+    -- актер
     person_name || ' ' || person_surname AS actor_name,
-    'best movie' AS actor_best_movie,
-    'rate' AS best_movie_rate,
-    'user' AS user_name
-FROM persons
-WHERE persons.id = 22
-;
+    (
+    -- название фильма
+    SELECT
+        m.title AS actor_best_movie
+    FROM
+        movies m
+    WHERE
+        m.id = (
+        SELECT
+            --  лучший фильм
+            s.movie_id
+        FROM
+            stars s
+        GROUP BY
+            s.movie_id
+        HAVING
+            s.movie_id IN (
+            SELECT
+                r.movie_id
+            FROM
+                roles r
+            WHERE
+                r.person_id = p.id)
+        ORDER BY
+            avg(s.number_of_stars) DESC
+        LIMIT 1
+            )
+    LIMIT 1
+        ),
+    (
+    -- звезды фильма
+    SELECT  
+         round( avg(s2.number_of_stars), 1) AS best_movie_rate
+    FROM
+        stars s2
+    WHERE
+        s2.movie_id = (
+        SELECT
+            --  лучший фильм
+            s.movie_id
+        FROM
+            stars s
+        GROUP BY
+            s.movie_id
+        HAVING
+            s.movie_id IN (
+            SELECT
+                r.movie_id
+            FROM
+                roles r
+            WHERE
+                r.person_id = p.id)
+        ORDER BY
+            avg(s.number_of_stars) DESC
+        LIMIT 1
+            )
+        )
+FROM
+    persons p
+WHERE
+    p.id IN (
+    SELECT
+        r2.person_id
+    FROM
+        roles r2
+    WHERE
+        r2.movie_id = (
+        SELECT
+            s3.movie_id
+        FROM
+            stars s3
+        WHERE
+            s3.user_id = (
+            SELECT
+                up.user_id
+            FROM
+                user_profiles up
+            WHERE
+                up."name" = 'Rooney'
+                AND up.surname = 'Cooley'
+            LIMIT 1)
+        ORDER BY
+            s3.rated_at DESC
+        LIMIT 1));
 
