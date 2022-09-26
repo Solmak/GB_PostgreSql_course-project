dbc88981da63a963272c652e4ade2b35feff3aaa
Solmak <solvy@internet.ru>
2022-09-26 12:53:32 +0300

    + stage 08

--- 08_Stage_Queries.sql
+++ 08_Stage_Queries.sql
@@ -1,3 +1,39 @@
 /*
 Создать пользовательскую функцию.
 */
+
+-- Функция вычисляет рейтинг фильма на основании оценок пользователей
+DROP FUNCTION IF EXISTS movie_rate;
+CREATE FUNCTION movie_rate(mv_id INTEGER)
+RETURNS REAL AS
+$$
+SELECT
+    avg(s.number_of_stars)
+FROM
+    stars s
+GROUP BY
+    s.movie_id
+HAVING 
+    s.movie_id = mv_id;
+
+$$
+LANGUAGE SQL;
+
+
+
+-- Процедура обновляет поле рейтинга всех фильмов на основании оценок пользователей
+CREATE OR REPLACE PROCEDURE set_movies_stars_rate()
+LANGUAGE PLPGSQL AS
+$$
+DECLARE movie_row RECORD;
+BEGIN
+FOR movie_row IN
+SELECT id
+FROM movies
+LOOP
+UPDATE movies  SET stars_rate  = movie_rate(movie_row.id)
+WHERE id = movie_row.id;
+END LOOP;
+COMMIT;
+END;
+$$;
